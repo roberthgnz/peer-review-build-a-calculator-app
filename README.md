46a99d663494ed19175235103734e2dc1af09851
roberthgnz <63687573+roberthgnz@users.noreply.github.com>
2023-09-15 22:48:03 +0200

    2 files modified, 1 deleted

--- README.md
+++
@@ -1,3 +0,0 @@
-# peer-review-build-a-calculator-app
-
-[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-fw87md)
--- src/App.js
+++ src/App.js
@@ -1,11 +1,50 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+
+import './style.css';
 
 export default function App() {
+  const [result, setResult] = React.useState(0);
+  const [number, setNumber] = React.useState(0);
+
+  const calculate = (act) => {
+    switch (act) {
+      case 'add':
+        setResult((prev) => prev + number);
+        break;
+      case 'subtract':
+        setResult((prev) => prev - number);
+        break;
+      case 'multiply':
+        setResult((prev) => prev * number);
+        break;
+      case 'divide':
+        setResult((prev) => prev / number);
+        break;
+    }
+  };
+
   return (
     <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+      <h1>Simplest Working Calculator</h1>
+      <p>{result}</p>
+      <input
+        type="number"
+        value={number}
+        onChange={(e) => setNumber(Number(e.target.value))}
+        required
+      />
+      <p className="controls">
+        <button onClick={() => calculate('add')}>add</button>
+        <button onClick={() => calculate('subtract')}>subtract</button>
+        <button onClick={() => calculate('multiply')}>multiply</button>
+        <button onClick={() => calculate('divide')}>divide</button>
+        <button className="special" onClick={() => setNumber(0)}>
+          reset input
+        </button>
+        <button className="special" onClick={() => setResult(0)}>
+          reset result
+        </button>
+      </p>
     </div>
   );
 }
